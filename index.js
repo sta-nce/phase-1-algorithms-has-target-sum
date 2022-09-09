85e55e4c2e8b979923ae4136f5f4108090ca3923
Stanslous <stancemuindi21@gmail.com>
2022-09-09 07:41:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  let nums ={}
+  for (let i=0; i<array.length; i++) {
+    let num = array[i];
+    if(target-num in nums){
+      return true;
+    }
+    nums[array[i]]=i;
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(N) wher N is the total number of elements in te given array
+  time complexity we get it by taking 0(n)
+  space complexity is gotten by 0(1) 
 */
 
 /* 
@@ -12,6 +24,9 @@ function hasTargetSum(array, target) {
 
 /*
   Add written explanation of your solution here
+  we assign a variable to an empty object
+  do a for loop
+  we search for y 
 */
 
 // You can run `node index.js` to view these console logs
